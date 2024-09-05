3e887e1ed3640c292e0cf51935f3c74b6c4f9772
fuyuko <fuyuko@users.noreply.github.com>
2024-09-05 11:49:07 -0500

    solved Valid Parentheses problem - needs improvement.

---
+++ valid-parentheses.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+/*
+Problem: https://leetcode.com/problems/valid-parentheses/?envType=study-plan-v2&envId=top-interview-150
+Lesson learned: 
+- Not very efficient solution. I need to revisit
+*/
+
+$solution = new Solution();
+$s = "([])";
+$result = $solution->isValid($s);
+if($result){
+    echo "is valid.";
+}else{
+    echo "is not valid";
+}
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Boolean
+     *
+     */
+    function isValid($s) {
+        $stack = [];
+
+        for($x = 0; $x < strlen($s); $x++){
+            switch(substr($s, $x, 1)){
+                case "(":
+                case "[":
+                case "{":
+                    array_unshift($stack, substr($s, $x, 1));
+                    break;
+                case ")":
+                    if($stack[0] != "("){
+                        return false;
+                    }
+                    array_shift($stack); 
+                    break;
+                case "]":
+                    if($stack[0] != "["){
+                        return false;
+                    }
+                    array_shift($stack);  
+                    break;
+                case "}":
+                    if($stack[0] != "{"){
+                        return false;
+                    }
+                    array_shift($stack); 
+                    break;
+            }       
+        }
+
+        if($stack){
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * @param String $s
+     * @return Boolean
+     *
+     * Worse runtime, better memory usage
+     * Rumtime 46ms beats 5.27%, Memory 20.40mb beats 27.42%
+     */
+    function isValidStillInefficient($s) {
+        $stack = [];
+
+        for($x = 0; $x < strlen($s); $x++){
+            switch(substr($s, $x, 1)){
+                case "(":
+                case "[":
+                case "{":
+                    array_unshift($stack, substr($s, $x, 1));
+                    break;
+                case ")":
+                    if($stack[0] != "("){
+                        return false;
+                    }
+                    array_shift($stack); 
+                    break;
+                case "]":
+                    if($stack[0] != "["){
+                        return false;
+                    }
+                    array_shift($stack);  
+                    break;
+                case "}":
+                    if($stack[0] != "{"){
+                        return false;
+                    }
+                    array_shift($stack); 
+                    break;
+            }       
+        }
+
+        if($stack){
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * @param String $s
+     * @return Boolean
+     *
+     * Rumtime 34ms beats 7.21%, Memory 20.68mb beats 8.61%
+     */
+    function isValidInefficient($s) {
+        $stack = [];
+
+        foreach(str_split($s) as $c){
+            switch($c){
+                case "(":
+                case "[":
+                case "{":
+                    array_unshift($stack, $c);
+                    break;
+                case ")":
+                    if($stack[0] != "("){
+                        return false;
+                    }
+                    array_shift($stack); 
+                    break;
+                case "]":
+                    if($stack[0] != "["){
+                        return false;
+                    }
+                    array_shift($stack);  
+                    break;
+                case "}":
+                    if($stack[0] != "{"){
+                        return false;
+                    }
+                    array_shift($stack); 
+                    break;
+            }       
+        }
+
+        if($stack){
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * @param String $s
+     * @return Boolean
+     * 
+     * This solution ignores the nesting order of the prentheses
+     */
+    function isValidIngoreNest($s) {
+        $track = array("(" => 0, "{" => 0, "[" => 0);
+
+        foreach(str_split($s) as $c){
+            //echo "c = " . $c;
+            switch($c){
+                case "(":
+                case "[":
+                case "{":
+                    $track[$c]++;
+                    break;
+                case ")":
+                    $track["("]--;
+                    break;
+                case "]":
+                    $track["["]--;
+                    break;
+                case "}":
+                    $track["{"]--;
+                    break;
+                default:
+                    break;
+            }
+            //var_dump($track);
+        }
+
+        foreach($track as $element){
+            if($element != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+?>
+
+</body>
+</html>
