aacac2676a81732fa5f30728bcce2982696ff1cb
fuyuko <fuyuko@users.noreply.github.com>
2024-10-22 09:04:05 -0500

    solved Kth Largest Element In Arra problem.

--- kth-largest-element-in-an-array.php
+++ kth-largest-element-in-an-array.php
@@ -7,7 +7,7 @@
 /*
 Problem: https://leetcode.com/problems/kth-largest-element-in-an-array/
 Lesson learned: 
-- 
+- Not a heap solution but a hashmap solution
 -
 */
 
@@ -28,19 +28,19 @@ class Solution {
         if(count($nums) === 1) return $nums[0];
         $min = $nums[0];
         $max = $nums[0];
-        $heap[$nums[0]] = 1;
+        $hashmap[$nums[0]] = 1;
 
         for($i=1; $i < count($nums); $i++){
             if($nums[$i] > $max) $max = $nums[$i];
             if($nums[$i] < $min) $min = $nums[$i];
-            if($heap[$nums[$i]] === null) $heap[$nums[$i]] = 1;
-            else $heap[$nums[$i]] += 1;
+            if($hashmap[$nums[$i]] === null) $hashmapp[$nums[$i]] = 1;
+            else $hashmap[$nums[$i]] += 1;
         } 
 
         $count = 0;
         for($i=$max; $i >= $min; $i--){
-            if($heap[$i] !== null){
-                $count += $heap[$i];
+            if($hashmap[$i] !== null){
+                $count += $hashmap[$i];
                 if($count >= $k) return $i;
             }
         }
